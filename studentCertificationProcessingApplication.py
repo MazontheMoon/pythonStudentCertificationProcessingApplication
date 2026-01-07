9c3b7c2ffa2ac73e776ac79fc4ad4ebf1467187e
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2026-01-07 14:20:50 +0000

    Add student certification processing application
    init python application with displayGreeting method

---
+++ studentCertificationProcessingApplication.py
@@ -0,0 +1,16 @@
+# SD-TA-010-B - Assignment
+# Author: Mary Ronan
+# Student Certification Processing Application
+
+main():
+
+    # Display greeting
+    displayGreeting()
+
+# Display greeting
+displayGreeting():
+    print("=============================================================")
+    print("Welcome to GTI's Student Certification Processing application")
+    print("=============================================================")
+
+run main()
